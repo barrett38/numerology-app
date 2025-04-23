98827237b8b885e8892a485f60f9af24352d3758
Barrett Anderson <barrett1614@gmail.com>
2025-04-23 13:25:18 -0500

    Working on Components

--- src/components/NamePrompt.js
+++ src/components/NamePrompt.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 export default function NamePrompt({ onSubmit }) {
   const [name, setName] = React.useState("");
 
--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,8 +1,29 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import FadeWrapper from "../components/FadeWrapper";
+import NamePrompt from "../components/NamePrompt";
+import BirthdayPrompt from "../components/BirthdayPrompt";
+
 export default function StartPage() {
+  const [step, setStep] = useState(1);
+  const [name, setName] = useState("");
+  const [birthday, setBirthday] = useState("");
+  const navigate = useNavigate();
+
+  const handleNameSubmit = (enteredName) => {
+    setName(enteredName);
+    setStep(2);
+  };
+
+  const handleBirthdaySubmit = (enteredBirthday) => {
+    setBirthday(enteredBirthday);
+    navigate("/results", { state: { name, birthday } });
+  };
+
   return (
-    <div className="start-page">
-      <h1>Welcome to the Start Page</h1>
-      <p>This is the starting point of our application.</p>
-    </div>
+    <FadeWrapper>
+      {step === 1 && <NamePrompt onSubmit={handleNameSubmit} />}
+      {step === 2 && <BirthdayPrompt onSubmit={handleBirthdaySubmit} />}
+    </FadeWrapper>
   );
 }
